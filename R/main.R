c07c86b2514db96880f30462010341031a74e200
Juan A. Botia <juanbot@um.es>
2019-03-13 23:23:17 +0100

    New big update

--- R/main.R
+++ R/main.R
@@ -16,20 +16,45 @@ initDb = function(mandatory=F){
               "THAL", "WHMT")
   nets = residuals = NULL
   for(tissue in tissues){
-    nets = c(nets, paste0("net",tissue,".12.signed.it.20.rds_cor_pca"))
+    nets = c(nets, paste0("net",tissue,".12.signed.it.20.rds_cor_pca.rds"))
     residuals = c(residuals, paste0(tissue,".mic.expr.data.19K.rds"))
   }
   
   for(tissue in tissues){
     net = nets[which(tissues == tissue)]
     residual = residuals[which(tissues == tissue)]
-    CoExpNets::addNet(which.one="CoExp10UKBEC",
+    CoExpNets::addNet(which.one="10UKBEC",
                       tissue=tissue,
-                      netfile=net,
-                      ctfile=paste0(the.dir,"/",net,".USER_terms.csv"),
+                      netfile=paste0(the.dir,"/",net),
+                      ctfile=paste0(the.dir,"/",net,".celltype.csv"),
                       gofile=paste0(the.dir,"/",net,"_gprofiler.csv"),
-                      exprdatafile=paste0(the.dir,"/",
-                                          residual),
+                      exprdatafile=paste0(the.dir,"/",residual),
                       overwrite=mandatory)
+  }
+}
+
+#' Title
+#'
+#' @param probes 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fromIlluminaProbe2GeneSymbol = function(probes){
+  probesdata = read.csv(paste0(system.file("micro19K/", "", package = "CoExp10UKBEC"),
+                                 "annot_19K.csv"),stringsAsFactors=F)
+  return(probesdata$Gene_Symbol[match(probes,probesdata$XtID)])
+}
+
+reannotateCellType = function(){
+  tissues = tissues = c("CRBL", "FCTX", "HIPP", "MEDU", "OCTX", "PUTM", "SNIG", "TCTX", 
+                        "THAL", "WHMT")
+  for(tissue in tissues){
+    net = getNetworkFromTissue(which.one="10UKBEC",tissue=tissue)
+    names(net$moduleColors) = fromIlluminaProbe2GeneSymbol(names(net$moduleColors))
+    write.csv(genAnnotationCellType(net.in=net,return.processed=F),paste0("~/Dropbox/KCL/workspace/CoExp10UKBEC/inst/micro19K/",
+                                                                          basename(getNetworkFromTissue(tissue=tissue,which.one="10UKBEC",only.file = T)),
+                                                                          ".celltype.csv"))
   }
 }
